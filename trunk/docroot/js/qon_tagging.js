00029ea23b9b20b32477bca631021d2bfb5aedb0
Jim Carroll <jim@maplesong.com>
2010-08-15 17:44:17 -0400

    updated tagging with link insertion first pass.

--- trunk/docroot/js/qon_tagging.js
+++ trunk/docroot/js/qon_tagging.js
@@ -29,8 +29,72 @@ onPageLoad = function() {
         var elem = elems[i];
         MochiKit.Signal.connect(elem, "onfocus", onTagFocus);
     }
+    
+    showControlMenu();
 }
 MochiKit.Signal.connect(window, "onload", onPageLoad);
+
+controlCell = function() {
+    var commentEntry = MochiKit.DOM.getElementsByTagAndClassName("textarea")[0];
+    //MochiKit.DOM.insertSiblingNodesAfter(commentEntry, helpers); // if span above
+    var nextCell = commentEntry.parentElement.nextElementSibling;
+    return nextCell;
+}
+
+showControlMenu = function() {
+    var d = MochiKit.DOM;
+    var helpers = d.TD({id:"comment_help"}, 
+      d.A({'onclick':'showInsertLink()', 'href':'javascript:void(0);'}, 'Insert link'), d.BR(),
+      d.A({'onclick':'showInsertImage()', 'href':'javascript:void(0);'}, 'Upload image and insert link'));
+    var cs = controlCell();
+    MochiKit.DOM.swapDOM(cs, helpers);
+}
+
+row_display = function (row) {
+    return TR(null, map(partial(TD, null), row));
+}
+
+showInsertLink = function(event) {
+    var d = MochiKit.DOM;
+    var ta = MochiKit.DOM.getElementsByTagAndClassName("textarea")[0];
+    var selected = '';
+    if (ta.selectionBegin != ta.selectionEnd)
+    {
+        selected = ta.value.slice(ta.selectionBegin, ta.selectionEnd);
+    }
+    
+    var helpers = d.TD({id:"link_inserter"}, 
+      d.TABLE(null, 
+          TR(null, TD(null, "Text to show:"), TD(null, d.INPUT({'type':'text', 'id':'new_showas', 'name': 'show_as'})) ),
+          TR(null, TD(null, "Link:"), TD(null, d.INPUT({'type':'text', 'id':'new_link', 'name': 'link'})) )
+      ),
+      d.A({'onclick':'showControlMenu()', 'href':'javascript:void(0);'}, 'Not Just Yet'), " ",
+      d.A({'onclick':'insertLink()', 'href':'javascript:void(0);'}, 'Insert Link') 
+      );
+    var cs = controlCell();
+    MochiKit.DOM.swapDOM(cs, helpers);
+}
+
+insertLink = function(event) {
+    var link = MochiKit.DOM.getElement("new_link").value;
+    if (link.slice(0, 7) != "http://")
+    {
+        link = "http://" + link;
+    }
+    var showas = MochiKit.DOM.getElement("new_showas").value;
+    var ta = MochiKit.DOM.getElementsByTagAndClassName("textarea")[0];
+    var cursorPos = ta.selectionEnd;
+    var oldText = ta.value;
+    var newText = oldText.slice(0, cursorPos) + '`' + showas + '`_'
+     + oldText.slice(cursorPos) + " "
+     + "\n.. _`" + showas + "`: " + link;
+    ta.value = newText;
+    showControlMenu();
+}
+
+showInsertImage = function(event) {
+    alert('inserting');
+}
 
 onTagFocus = function(event) {
     tools_area_id = this.attributes["tools_id"].value;
